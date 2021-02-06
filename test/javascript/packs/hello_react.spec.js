ed032f1c4154f140ef48953cacdd6ed67a5beb86
David O'Keefe <david.okeefe@salesloft.com>
2021-02-06 15:41:41 -0500

    Setup React

---
+++ test/javascript/packs/hello_react.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import HelloReact from 'packs/hello_react'
+
+describe('HelloReact component', () => {
+  describe('when a name is given as a prop', () => {
+    it('render Hello Caique!', () => {
+      expect(shallow().text()).toBe('Hello Caique!')
+    })
+  })
+
+  describe('when no name is given', () => {
+    it('render Hello David!', () => {
+      expect(shallow().text()).toBe('Hello David!')
+    })
+  })
+})
