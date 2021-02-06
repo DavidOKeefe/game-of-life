1995b2324c8e589a10701c993e955defa51949a2
chae <chaeokeefe@watchtowerbenefits.com>
2021-02-06 15:35:18 -0500

    Seeds Categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  NAMES = {
+    hard:   'hard',
+    medium: 'medium',
+    easy:   'easy'
+  }
+
   validates :name,
             presence:   true,
             uniqueness: true
