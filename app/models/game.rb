0354d75c8c2e05436ff6ba24adb27379ab4568f2
chae <chaeokeefe@watchtowerbenefits.com>
2021-02-06 15:26:59 -0500

    Formatting

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Game < ApplicationRecord
-  has_many :scores, inverse_of: :game, dependent: :destroy
+  has_many :scores,
+           inverse_of: :game,
+           dependent:  :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name,
+            presence:   true,
+            uniqueness: true
 end
