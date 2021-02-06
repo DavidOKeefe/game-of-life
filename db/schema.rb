cfa938ad71d908e2adefe877f283a253a09cdc75
chae <chaeokeefe@watchtowerbenefits.com>
2021-02-06 17:17:02 -0500

    Migration - Games belong to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_204850) do
+ActiveRecord::Schema.define(version: 2021_02_06_221323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 2021_02_06_204850) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["name"], name: "index_games_on_name"
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "scores", force: :cascade do |t|
@@ -51,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_02_06_204850) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
   add_foreign_key "scores", "categories"
   add_foreign_key "scores", "games"
 end
